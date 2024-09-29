253e6da421fade1caa3444c53f6ceb624a770a4d
my_name <myintnaing.phone@my.jcu.edu.au>
2024-09-29 18:17:02 +0800

    score changes

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -5,12 +5,21 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
+def main():
+    score = float(input("Enter score: "))
+    get_score_category(score)
+
+
+def get_score_category(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
         print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
+
+
