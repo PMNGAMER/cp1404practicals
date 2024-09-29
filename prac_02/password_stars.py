34cb5b1dc18d1b14cdbcb6ede0a4e1feb315405b
my_name <myintnaing.phone@my.jcu.edu.au>
2024-09-29 18:05:59 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,1 +1,9 @@
-print("Welcome to VCS!")
+MINIMUM_LENGTH = 5
+password = input("Enter a password: ")
+
+while len(password) < MINIMUM_LENGTH:
+    print(f"Password must be at least {MINIMUM_LENGTH} characters long".format(MINIMUM_LENGTH))
+    password = input("Enter a password: ")
+
+asterisks = "*" * len(password)
+print(asterisks)
