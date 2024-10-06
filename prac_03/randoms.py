e15b18536448a3125e0e2eac511c5a39939ffbbc
my_name <myintnaing.phone@my.jcu.edu.au>
2024-10-06 14:53:00 +0800

    randoms.py code console testing

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+#A Line of Code that generates a random number between 5 and 20
+#Smallest Number is 5 and the Largest Number is 20
+
+print(random.randrange(3, 10, 2))  # line 2
+#"This line generates a random number between 3 and 10 with a step of 2"
+# "The Smallest Number is 3 and the Largest Number is 9"
+#"4 is not part of the sequence as the step is only 2"
+print(random.uniform(2.5, 5.5))  # line 3
+#"This line generates a random number between 2.5 and 5.5"
+#"The smallest number is 2.5 and the largest number is 5.5"
