191e2aa125fcff9e57f6ca1b0fe53a369726b0f1
my_name <myintnaing.phone@my.jcu.edu.au>
2024-10-20 22:27:39 +0800

    commiting cuz i forgot to commit

---
+++ prac_05/seminar.py
@@ -0,0 +1,6 @@
+"""name_and_number = {'Derek':7, "Xavier":80, "Bob":612, "Chantanelle":9}
+
+for name, value in name_and_number.items():
+    print(f"{name: <12} = {value: >3}")"""
+
+print("*".join([len(word) for word in "one*two*three".split('*')]))
